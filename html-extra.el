62c29bc11ab8c9805ecdc5fe77a2a39f7d57252f
AbyChan <crystal.tyan@gmail.com>
2016-01-26 10:38:30 +0800

    卡出翔

--- html-extra.el
+++ html-extra.el
@@ -30,41 +30,41 @@
   (set (make-local-variable 'yas-fallback-behavior)
        '(apply simplezen-expand-or-indent-for-tab)))
 
-(add-hook 'sgml-mode-hook '--setup-simplezen)
-(add-hook 'html-mode-hook '--setup-simplezen)
+;; (add-hook 'sgml-mode-hook '--setup-simplezen)
+;; (add-hook 'html-mode-hook '--setup-simplezen)
 
-(eval-after-load "sgml-mode"
-  '(progn
-     ;; don't include equal sign in symbols
-     (modify-syntax-entry ?= "." html-mode-syntax-table)
+;; (eval-after-load "sgml-mode"
+;;   '(progn
+;;      ;; don't include equal sign in symbols
+;;      (modify-syntax-entry ?= "." html-mode-syntax-table)
 
-     (define-key html-mode-map [remap forward-paragraph] 'skip-to-next-blank-line)
-     (define-key html-mode-map [remap backward-paragraph] 'skip-to-previous-blank-line)
-     (define-key html-mode-map (kbd "C-c C-w") 'html-wrap-in-tag)
-     (define-key html-mode-map (kbd "/") nil) ;; no buggy matching of slashes
+;;      (define-key html-mode-map [remap forward-paragraph] 'skip-to-next-blank-line)
+;;      (define-key html-mode-map [remap backward-paragraph] 'skip-to-previous-blank-line)
+;;      (define-key html-mode-map (kbd "C-c C-w") 'html-wrap-in-tag)
+;;      (define-key html-mode-map (kbd "/") nil) ;; no buggy matching of slashes
 
-     (define-key html-mode-map (kbd "C-c C-d") 'ng-snip-show-docs-at-point)
+;;      (define-key html-mode-map (kbd "C-c C-d") 'ng-snip-show-docs-at-point)
 
-     (require 'tagedit)
+;;      (require 'tagedit)
 
-     ;; paredit lookalikes
-     (define-key html-mode-map (kbd "s-<right>") 'tagedit-forward-slurp-tag)
-     (define-key html-mode-map (kbd "C-)") 'tagedit-forward-slurp-tag)
-     (define-key html-mode-map (kbd "s-<left>") 'tagedit-forward-barf-tag)
-     (define-key html-mode-map (kbd "C-}") 'tagedit-forward-barf-tag)
-     (define-key html-mode-map (kbd "M-r") 'tagedit-raise-tag)
-     (define-key html-mode-map (kbd "s-s") 'tagedit-splice-tag)
-     (define-key html-mode-map (kbd "M-S") 'tagedit-split-tag)
-     (define-key html-mode-map (kbd "M-J") 'tagedit-join-tags)
-     (define-key html-mode-map (kbd "M-?") 'tagedit-convolute-tags)
+;;      ;; paredit lookalikes
+;;      (define-key html-mode-map (kbd "s-<right>") 'tagedit-forward-slurp-tag)
+;;      (define-key html-mode-map (kbd "C-)") 'tagedit-forward-slurp-tag)
+;;      (define-key html-mode-map (kbd "s-<left>") 'tagedit-forward-barf-tag)
+;;      (define-key html-mode-map (kbd "C-}") 'tagedit-forward-barf-tag)
+;;      (define-key html-mode-map (kbd "M-r") 'tagedit-raise-tag)
+;;      (define-key html-mode-map (kbd "s-s") 'tagedit-splice-tag)
+;;      (define-key html-mode-map (kbd "M-S") 'tagedit-split-tag)
+;;      (define-key html-mode-map (kbd "M-J") 'tagedit-join-tags)
+;;      (define-key html-mode-map (kbd "M-?") 'tagedit-convolute-tags)
 
-     (tagedit-add-experimental-features)
-     (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))
-     ;;(add-hook 'web-mode-hook (lambda () (tagedit-mode 1))) ;;有故障
+;;      (tagedit-add-experimental-features)
+;;      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))
+;;      ;;(add-hook 'web-mode-hook (lambda () (tagedit-mode 1))) ;;有故障
 
-     ;; no paredit equivalents
-     (define-key html-mode-map (kbd "M-k") 'tagedit-kill-attribute)
-     (define-key html-mode-map (kbd "s-<return>") 'tagedit-toggle-multiline-tag)))
+;;      ;; no paredit equivalents
+;;      (define-key html-mode-map (kbd "M-k") 'tagedit-kill-attribute)
+;;      (define-key html-mode-map (kbd "s-<return>") 'tagedit-toggle-multiline-tag)))
 
 ;; after deleting a tag, indent properly
 (defadvice sgml-delete-tag (after reindent activate)
